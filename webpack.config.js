96a444c5d54f97836ee89af6d3ca3a30d71492dc
Andrey <plotnik1992@gmail.com>
2023-01-02 14:31:20 +0700

    Start project

--- webpack.config.js
+++ webpack.config.js
@@ -3,19 +3,28 @@ const HTMLWebpackPlugin = require('html-webpack-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 
 module.exports = {
+    context: path.resolve(__dirname, 'src'),
     mode: 'development',
     entry: {
-        main: './src/index.js',
-        analytics: './src/analytics.js'
+        main: './index.js',
+        analytics: './analytics.js'
     },
     output: {
         filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist')
     },
     plugins: [
-        new HTMLWebpackPlugin({           
-            template: './src/index.html'
+        new HTMLWebpackPlugin({
+            template: './index.html'
         }),
         new CleanWebpackPlugin()
-    ]
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        }
+      ]
+    }
 }
